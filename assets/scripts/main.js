766ccf0b54468e9b0741637a853ee4c23bf414bb
Hans Pattnaik <64223974+HANS-2002@users.noreply.github.com>
2022-06-28 19:39:55 +0530

    Added block breaker

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,7 @@
 var themeBtn1 = document.querySelector(".themeBtn1");
 var themeBtn2 = document.querySelector(".themeBtn2");
 var themeLink = document.querySelector("#themeLink");
+var workImage = document.querySelector(".workImage");
 
 function changeTheme1(){
     if(themeBtn1.classList.contains('fa-moon')){
@@ -50,3 +51,30 @@ function themeUpdate(){
         themeLink.href = "assets/styles/lightTheme.css";
     }
 }
+
+function forceDownload(blob, filename) {
+    var a = document.createElement('a');
+    a.download = filename;
+    a.href = blob;
+    // For Firefox https://stackoverflow.com/a/32226068
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+  }
+  
+  // Current blob size limit is around 500MB for browsers
+  function downloadResource(url, filename) {
+    if (!filename) filename = url.split('\\').pop().split('/').pop();
+    fetch(url, {
+        headers: new Headers({
+          'Origin': location.origin
+        }),
+        mode: 'cors'
+      })
+      .then(response => response.blob())
+      .then(blob => {
+        let blobUrl = window.URL.createObjectURL(blob);
+        forceDownload(blobUrl, filename);
+      })
+      .catch(e => console.error(e));
+  }
